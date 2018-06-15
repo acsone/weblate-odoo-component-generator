a389f4828cba7288999422354c694c5a23c0b7e9
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-06-15 19:39:08 +0200

    use project template more
    In particular copy, access control level.

--- wocg/create_project.py
+++ wocg/create_project.py
@@ -77,10 +77,20 @@ def get_new_project(project_name, repository, tmpl_component_slug):
     new_project.name = project_name
     new_project.slug = get_project_slug(project_name)
     new_project.web = giturlparse.parse(repository).url2https
+    new_project.access_control = \
+        tmpl_component.project.access_control
     new_project.enable_review = \
         tmpl_component.project.enable_review
+    new_project.enable_hooks = \
+        tmpl_component.project.enable_hooks
     new_project.set_translation_team = \
         tmpl_component.project.set_translation_team
+    new_project.source_language = \
+        tmpl_component.project.source_language
+    new_project.instructions = \
+        tmpl_component.project.instructions
+    new_project.mail = \
+        tmpl_component.project.mail
     new_project.save()
     return new_project
 
