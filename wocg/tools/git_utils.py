4ee347dcc7ab30d046d6a1737c4088738ebccc4a
Benjamin Willig <benjamin.willig@acsone.eu>
2018-09-14 16:45:10 +0200

    [IMP] allow to use SSH protocols to clone repositories when creating projects

--- wocg/tools/git_utils.py
+++ wocg/tools/git_utils.py
@@ -9,13 +9,17 @@ import giturlparse
 
 
 @contextlib.contextmanager
-def temp_git_clone(repository, branch):
-    repository_https = giturlparse.parse(repository).url2https
+def temp_git_clone(repository, branch, use_ssh=False):
+    parsed_repository_url = giturlparse.parse(repository)
+    if use_ssh:
+        repository_url = parsed_repository_url.url2ssh
+    else:
+        repository_url = parsed_repository_url.url2https
     with tempfile.TemporaryDirectory() as repo_dir:
         subprocess.check_call([
             'git',
             'clone',
-            repository_https,
+            repository_url,
             '-b', branch,
             '--depth', '1',
             repo_dir,
