bbf18d4a9139954aa8262e0976755672a92d79fd
RohmerJay <103479046+RohmerJay@users.noreply.github.com>
2022-06-08 18:33:53 +0800

    Please update to the latest version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     use_scm_version=True,
     packages=find_packages(),
     install_requires=[
-        "weblate==4.3.*",
+        "weblate==4.12.*",
         'click',
         'giturlparse',
     ],
